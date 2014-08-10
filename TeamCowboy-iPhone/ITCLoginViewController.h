d1841a90f7bcf8e4e049cf7aae01594b8a330f06
Nick Ciaravella <nick.ciaravella@live.com>
2014-08-10 14:00:15 -0700

    Adding view for the login page with a stub for the controller. Changing the login storyboard to be the initial. This should be changed back and conditionally shown depending if there is a logged in user on app start.

---
+++ TeamCowboy-iPhone/ITCLoginViewController.h
@@ -0,0 +1,13 @@
+//
+//  ITCLoginViewController.h
+//  Copyright (c) 2014 Nick Ciaravella. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+/**
+ @details Controller for the login page.
+ */
+@interface ITCLoginViewController : UIViewController
+
+@end
---
+++ TeamCowboy-iPhone/ITCLoginViewController.m
@@ -0,0 +1,12 @@
+//
+//  ITCLoginViewController.m
+//  Copyright (c) 2014 Nick Ciaravella. All rights reserved.
+//
+
+#import "ITCLoginViewController.h"
+
+#pragma mark - ITCLoginViewController (implementation)
+
+@implementation ITCLoginViewController
+
+@end
