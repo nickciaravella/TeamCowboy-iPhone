de0b3f310b32059f36b7f498d09ae82580fbb898
Nick Ciaravella <nick.ciaravella@live.com>
2014-10-04 14:36:10 -0700

    Adding showing a mail controller when the "Feedback" button is clicked. Just need to update the subject and "to" once app name / email address are finalized.

--- TeamCowboy-iPhone/ITCMoreTableViewController.m
+++ TeamCowboy-iPhone/ITCMoreTableViewController.m
@@ -5,6 +5,15 @@
 
 #import "ITCMoreTableViewController.h"
 
+#pragma mark - ITCMoreTableViewController ()
+
+@interface ITCMoreTableViewController () <MFMailComposeViewControllerDelegate>
+
+@end
+
+
+#pragma mark - ITCMoreTableViewController (implementation)
+
 @implementation ITCMoreTableViewController
 
 #pragma mark - ITCMoreTableViewController
@@ -31,10 +40,37 @@ didSelectRowAtIndexPath:(NSIndexPath *)indexPath
         case 0:
             [self.delegate moreControllerDidClickSignOutButton:self];
             break;
-            
+        case 1:
+            [self feedbackButtonClicked];
         default:
             break;
     }
+    
+    [self.tableView deselectRowAtIndexPath:indexPath animated:NO];
+}
+
+#pragma mark - MFMailComposeViewControllerDelegate
+
+//
+//
+- (void)mailComposeController:(MFMailComposeViewController *)controller
+          didFinishWithResult:(MFMailComposeResult)result
+                        error:(NSError *)error
+{
+    [self dismissViewControllerAnimated:YES completion:nil];
+}
+
+#pragma mark - Private
+
+//
+//
+- (void)feedbackButtonClicked
+{
+    MFMailComposeViewController *mailController = [MFMailComposeViewController new];
+    [mailController setToRecipients:@[ @"nick.ciaravella@live.com" ]];
+    [mailController setSubject:@"Feedback on your iOS app"];
+    [mailController setMailComposeDelegate:self];
+    [self presentViewController:mailController animated:YES completion:nil];
 }
 
 @end
